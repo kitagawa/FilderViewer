5f0bfa2536b08df9cfdf4d7500fcb045e3b5ac63
m.kitagawa <kitagawa@.(none)>
2012-03-10 19:33:56 +0900

    first commit created base rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilderViewer::Application.config.secret_token = '729f0e1d248af35216ca98fa4a7407baf23cea97309964311cc7631eab86f6bb7916f112c817ca1e8f17593764c146807bdb99020ab25e86df0356753212584d'
